cc94a32cd8db22f66ee04595f4cf626162065d30
machine-sg <60744373+machine-sg@users.noreply.github.com>
2020-12-27 20:29:03 +0530

    changed time

--- admin/test_created.js
+++ admin/test_created.js
@@ -1,7 +1,7 @@
 const startBtn = document.getElementById('start');
 //const overBtn = document.getElementById('over');
 const resultBtn = document.getElementById('result');
-var startDate = new Date("Dec 25, 2020 22:37:25").getTime(); 
+var startDate = new Date("Dec 27, 2020 22:37:25").getTime(); 
 
 var time = new Date().getTime();
 var t = startDate - time ;
