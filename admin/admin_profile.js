3bf0b21616bb57c20104b8c417c68226361e222a
machine-sg <60744373+machine-sg@users.noreply.github.com>
2021-01-26 16:45:45 +0530

    added profile image changing feature

---
+++ admin/admin_profile.js
@@ -0,0 +1,19 @@
+$("#profileImage").click(function(e) {
+    $("#imageUpload").click();
+});
+
+function fasterPreview( uploader ) {
+    if ( uploader.files && uploader.files[0] ){
+          $('#profileImage').attr('src', 
+             window.URL.createObjectURL(uploader.files[0]) );
+    }
+}
+
+$("#imageUpload").change(function(){
+    fasterPreview( this );
+});
+
+$(".remove").click(function(){
+    $(this).parent(".pip").remove();
+    $('#files').val("");
+});
